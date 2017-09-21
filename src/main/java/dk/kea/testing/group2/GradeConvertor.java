c9dc180d847994a6b0a10aeb130a3b7b577163f5
Cristiana Man <man.cristiana1@gmail.com>
2017-09-21 10:59:19 +0200

    first commit

---
+++ src/main/java/dk/kea/testing/group2/GradeConvertor.java
@@ -0,0 +1,10 @@
+package dk.kea.testing.group2;
+
+/**
+ * Created by mancr on 21-Sep-17.
+ */
+public class GradeConvertor {
+    public String danishToects(String danishGrade) {
+        return "";
+    }
+}
